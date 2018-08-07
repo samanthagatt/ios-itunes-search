310daaee6a4655aa84bdb97e5243057838b3f452
Samantha Gatt <smfgatt@gmail.com>
2018-08-07 16:11:29 -0400

    Configures table view controller

--- iTunes Search/iTunes Search/VCs/SearchResultsTableViewController.swift
+++ iTunes Search/iTunes Search/VCs/SearchResultsTableViewController.swift
@@ -8,22 +8,63 @@
 
 import UIKit
 
-class SearchResultsTableViewController: UITableViewController {
+class SearchResultsTableViewController: UITableViewController, UISearchBarDelegate {
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return searchResults.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchResultCell", for: indexPath)
 
-        
+        let searchResult = searchResults[indexPath.row]
+        cell.textLabel?.text = searchResult.title
+        cell.detailTextLabel?.text = searchResult.creator
 
         return cell
     }
     
+    // MARK: - SearchBarDelegate
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchTerm = searchBar.text else { return }
+        // Instructions said to make it a type ResultType! but I left off the ! because I didn't see why it was needed and it made .software etc not work
+        var resultType: ResultType
+        
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            resultType = .software
+        case 1:
+            resultType = .musicTrack
+        default:
+            resultType = .movie
+        }
+        
+        searchResultController.performSearch(for: searchTerm, resultType: resultType) { (searchResults, error) -> NSError? in
+            
+            if let error = error {
+                NSLog("Error performing search: \(error)")
+                return NSError()
+            }
+            
+            self.searchResults = searchResults ?? []
+            return nil
+        }
+    }
+    
+    
+    // MARK: - Properties
+    
+    let searchResultController = SearchResultController()
+    var searchResults = [SearchResult]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
     
     
     // MARK: - Outlets
