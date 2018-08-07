8284903231de4736f8e755cbeef6e30109c591f6
Samantha Gatt <smfgatt@gmail.com>
2018-08-07 14:39:30 -0400

    Configures result type enum

---
+++ iTunes Search/iTunes Search/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunes Search
+//
+//  Created by Samantha Gatt on 8/7/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
